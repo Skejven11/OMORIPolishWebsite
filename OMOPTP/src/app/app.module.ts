7d5698aa4cb2aa382ce251b8ac43f4bceca3ffdf
Skejven11 <45572843+Skejven11@users.noreply.github.com>
2022-05-26 18:41:54 +0200

    Add team content, more style adjusting

--- OMOPTP/src/app/app.module.ts
+++ OMOPTP/src/app/app.module.ts
@@ -3,7 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApplyComponent } from './screens/apply/apply.component';
+import { FaqComponent } from './screens/faq/faq.component';
+import { FrontpageComponent } from './screens/frontpage/frontpage.component';
 import { MainLayoutComponent } from './screens/main-layout/main-layout.component';
+import { TeamComponent } from './screens/team/team.component';
 import { NavbarComponent } from './screens/main-layout/navbar/navbar.component';
 import { LogoComponent } from './screens/main-layout/logo/logo.component';
 import { FooterComponent } from './screens/main-layout/footer/footer.component';
@@ -11,16 +15,22 @@ import { OmoButtonComponent } from './components/omo-button/omo-button.component
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuItemComponent } from './screens/main-layout/navbar/menu-item/menu-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TeamItemComponent } from './screens/team/team-item/team-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
+    ApplyComponent,
+    FaqComponent,
+    FrontpageComponent,
+    TeamComponent,
     NavbarComponent,
     LogoComponent,
     FooterComponent,
     OmoButtonComponent,
-    MenuItemComponent
+    MenuItemComponent,
+    TeamItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ OMOPTP/src/app/screens/team/team-item/team-item.component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { faDiscord, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+@Component({
+  selector: 'team-item',
+  templateUrl: './team-item.component.html',
+  styleUrls: ['./team-item.component.scss']
+})
+export class TeamItemComponent implements OnInit {
+  @Input() name: string = "";
+  @Input() roles: string = "";
+  @Input() twitterLink: string = "";
+  @Input() discordLink: string = "";
+  @Input() imgSrc: string = "";
+  public faDiscord = faDiscord;
+  public faTwitter = faTwitter;
+
+  constructor(
+    private changeDetector: ChangeDetectorRef,
+  ) { }
+
+  ngOnInit(): void {
+    this.imgSrc = this.imgSrc != "" ? "../../../../assets/img/" + this.imgSrc : "";
+    this.discordLink = this.discordLink != "" ? "https://discordapp.com/users/" + this.discordLink : "";
+    this.twitterLink = this.twitterLink != "" ? "https://twitter.com/" + this.twitterLink : "";
+    console.log(this.discordLink)
+    this.changeDetector.detectChanges();
+  }
+
+}
