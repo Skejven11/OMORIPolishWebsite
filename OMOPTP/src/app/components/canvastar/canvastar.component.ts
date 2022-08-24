c7cdef6c167ff71660605a75d1fd53d4403eb80d
Skejven11 <45572843+Skejven11@users.noreply.github.com>
2022-08-24 18:26:39 +0200

    Add star animation or bunny

--- OMOPTP/src/app/components/canvastar/canvastar.component.ts
+++ OMOPTP/src/app/components/canvastar/canvastar.component.ts
@@ -1,28 +1,87 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'canvastar',
   templateUrl: './canvastar.component.html'
 })
-export class CanvastarComponent implements OnInit {
+export class CanvastarComponent implements AfterViewInit {
   @ViewChild('canvas', {static: true}) canvas: ElementRef<HTMLCanvasElement>;
-  @ViewChild('starImage') starImage: HTMLImageElement;
+  @ViewChild('starImage') starImage: ElementRef;
   private ctx: CanvasRenderingContext2D;
+  private image: {
+    imageSrc: HTMLImageElement,
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    velocityX: number,
+    velocityY: number
+  }
+  private gravity: number = -2;
+  private alreadyLaunched: boolean = false;
+  private speed: number = 35;
+  private longevity: number = 30
 
   constructor(
   ) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.image = {
+      imageSrc: this.starImage.nativeElement,
+      width: 50,
+      height: 50,
+      x: 0,
+      y: 0,
+      velocityX: 0,
+      velocityY: 0
+    }
     this.ctx = this.canvas.nativeElement.getContext('2d');
   }
 
-  launchStar() {
-    this.ctx.drawImage(this.starImage, 50, 50)
+  async launchStar() {
+    if (this.alreadyLaunched) return;
+
+    let leftSide: boolean = this.randomiseNumber(3, 1) === 1 ? true : false;
+
+    this.image.x = this.canvas.nativeElement.width / 2;
+    this.image.y = this.canvas.nativeElement.height * 1/4;
+    this.image.velocityY = 10;
+    this.image.velocityX = leftSide ? -this.randomiseNumber(15, 7) : this.randomiseNumber(15, 7);
+    this.image.width = 50;
+    this.image.height = 50;
+
+    this.alreadyLaunched = true;
+    for (let i=0;i<this.longevity;i++) {
+        this.drawStar();
+        this.updateStar();
+        await this.wait(this.speed);
+      }
+    this.alreadyLaunched = false;
+    }
+
+  updateStar() {
+    this.image.velocityY += this.gravity;
+
+    this.image.x += this.image.velocityX;
+    this.image.y -= this.image.velocityY;
+    this.image.width--;
+    this.image.height--;
   }
 
-  randomiseArc() {
-    let x: number = 0;
-    x = Math.random() * (30 -1 + 1) + 1;
-    return x;
+  drawStar() {
+    this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+    this.ctx.drawImage(this.image.imageSrc, this.image.x, this.image.y, this.image.width, this.image.height);
   }
+
+  randomiseNumber(max: number, min: number) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  wait(ms) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+      resolve(ms)
+      }, ms )
+    })
+  }  
 }
--- OMOPTP/src/app/screens/frontpage/frontpage.component.ts
+++ OMOPTP/src/app/screens/frontpage/frontpage.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { BreakpointState } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ScreenWidthService } from 'src/app/common/screen-width.service';
 
 @Component({
   selector: 'frontpage',
   templateUrl: './frontpage.component.html',
 })
 export class FrontpageComponent implements OnInit {
+  public isBelowMd: boolean = false;
 
-  constructor() { }
+  constructor(
+    private screenWidthService: ScreenWidthService,
+    private changeDetector: ChangeDetectorRef
+  ) { }
 
   ngOnInit(): void {
+    this.screenWidthService.isBelowMd().subscribe((isBelowMd: BreakpointState) => {
+      this.isBelowMd = isBelowMd.matches;
+      this.changeDetector.detectChanges();
+    })
   }
 
 }
