2bbcea2347f9ebb53998d24c21daba09ea637fd6
Skejven11 <45572843+Skejven11@users.noreply.github.com>
2022-09-03 21:21:39 +0200

    Change omo-button to handle not rourter stuff, some parser things

--- OMOPTP/src/app/components/omo-button/omo-button.component.ts
+++ OMOPTP/src/app/components/omo-button/omo-button.component.ts
@@ -1,18 +1,27 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'omo-button',
   templateUrl: './omo-button.component.html',
 })
 export class OmoButtonComponent implements OnInit {
+  @Output() onButtonClick = new EventEmitter<any>();
   @Input() link: string = "";
   @Input() text: string = "";
   @Input() isActive: boolean = false;
   public isMouseOver: boolean = false;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onClick() {
+    if (this.link != "") this.router.navigate([this.link])
+    else this.onButtonClick.emit();
   }
 
 }
--- OMOPTP/src/app/screens/dialogue-parser/dialogue-parser.component.ts
+++ OMOPTP/src/app/screens/dialogue-parser/dialogue-parser.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-dialogue-parser',
@@ -6,10 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dialogue-parser.component.scss']
 })
 export class DialogueParserComponent implements OnInit {
+  public scriptText: string = "faceset: MainCharacters_DreamWorld faceindex: 39 text: \herHey, is it just me or does it seem quieter all of a sudden?";
+  @ViewChild('textBox') textBox: ElementRef;
 
-  constructor() { }
+  constructor(
+    private renderer: Renderer2
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  generateText() {
+    let span = this.renderer.createElement('span');
+    this.renderer.addClass(span, 's4');
+    this.renderer.appendChild(this.textBox.nativeElement, span)
+    this.findSet(this.scriptText);
+    this.findIndex(this.scriptText);
+    this.findText(this.scriptText);
+  }
+
+  findSet(script: string): string {
+    let startPosition: number = script.search("faceset")+9;
+    script = script.slice(startPosition)
+    let endPosition: number = script.search(" ");
+    script = script.slice(0, endPosition)
+    console.log(script)
+    return script;
+  }
+
+  findIndex(script: string): string {
+    let startPosition: number = script.search("faceindex")+11;
+    script = script.slice(startPosition)
+    let endPosition: number = script.search(" ");
+    script = script.slice(0, endPosition)
+    console.log(script)
+    return script;
+  }
+
+
+  findText(script: string): string {
+    let startPosition: number = script.search("text")+6;
+    script = script.slice(startPosition)
+    console.log(script)
+    return script;
   }
 
 }
