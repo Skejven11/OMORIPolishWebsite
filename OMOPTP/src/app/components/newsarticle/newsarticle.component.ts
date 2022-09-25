35ad468b6dcc4051c97ee294b8babbad7dba0ab5
Skejven11 <45572843+Skejven11@users.noreply.github.com>
2022-09-25 16:40:06 +0200

    Add modal window for photos

--- OMOPTP/src/app/components/newsarticle/newsarticle.component.ts
+++ OMOPTP/src/app/components/newsarticle/newsarticle.component.ts
@@ -1,5 +1,5 @@
 import { BreakpointState } from '@angular/cdk/layout';
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, HostListener, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { ScreenWidthService } from 'src/app/common/screen-width.service';
 
 @Component({
@@ -9,16 +9,36 @@ import { ScreenWidthService } from 'src/app/common/screen-width.service';
 })
 export class NewsarticleComponent implements OnInit {
   @Input() public article: NewsArticle;
+  @ViewChild('modalWindow') modalWindow: ElementRef;
+  @ViewChild('modalPhoto') modalPhoto: ElementRef;
+  public showPhoto: boolean = false;
+  public photoSrc: string = "";
 
   public isBelowMd: boolean = false;
 
   constructor(
-    private screenWidthService: ScreenWidthService
+    private screenWidthService: ScreenWidthService,
+    private renderer: Renderer2,
+    private changeDetector: ChangeDetectorRef
   ) { }
 
   ngOnInit(): void {
     this.screenWidthService.isBelowMd().subscribe((isBelowMd: BreakpointState) => {
       this.isBelowMd = isBelowMd.matches;
+    })
+  }
+
+  onPhotoClick(photoSrc: string):void { 
+    this.showPhoto = true;
+    this.photoSrc = photoSrc;
+    this.changeDetector.detectChanges();
+    var modalWindow = this.modalWindow.nativeElement;
+    var modalPhoto = this.modalPhoto.nativeElement;
+    var listener = this.renderer.listen(modalWindow, 'click', (e: Event) => {
+      if (e.target != modalPhoto) {
+        this.showPhoto = false;
+        listener();
+      }
     })
   }
 
