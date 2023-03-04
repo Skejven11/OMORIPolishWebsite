ef351e9936c0e7624de7ab97a5b316e942db5f41
Skejven11 <45572843+Skejven11@users.noreply.github.com>
2023-03-04 20:07:40 +0100

    News, mappi, tiktok link

--- OMOPTP/src/app/screens/main-layout/footer/footer.component.ts
+++ OMOPTP/src/app/screens/main-layout/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faDiscord, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faDiscord, faTwitter, faYoutube, faTiktok } from '@fortawesome/free-brands-svg-icons';
 import { Store } from '@ngrx/store';
 import { selectTheme } from 'src/app/state/theme.selector';
 
@@ -11,6 +11,7 @@ export class FooterComponent implements OnInit {
   public faDiscord = faDiscord;
   public faTwitter = faTwitter;
   public faYoutube = faYoutube;
+  public faTiktok = faTiktok;
 
   public appTheme$ = this.store.select(selectTheme);
 
--- OMOPTP/src/app/screens/news/articles-provider.ts
+++ OMOPTP/src/app/screens/news/articles-provider.ts
@@ -2,17 +2,43 @@ import { NewsArticle } from "src/app/components/newsarticle/newsarticle.componen
 
 export var articles: NewsArticle[] = [
     {
+        date: '04.03.2023',
+        author: 'Yukki',
+        text: `Dzień dobry Cebulki!<br>
+        Jeśli brak wam energii (w szczególności z rana), zaleca się przesłuchanie „Good Morning” w 
+        <a href="https://www.youtube.com/watch?v=-CYHgPclI-g" class="text-red-500">wykonaniu naszego zespołu</a>. 9/10 lekarzy potwierdza wysoką skuteczność tej metody.<br>
+        Kiedy szefowa wyszła na ciastko, po kryjomu zaśpiewaliśmy „Good Morning”. Ale co to by było za spolszczenie, gdyby tekst byłby po angielsku? 
+        Nie martwcie się, o to też zadbaliśmy, a głównie Oskar. Gdy szefowa wróciła i już miała nam spuścić manto za obijanie się, usłyszała śpiew swojej siły roboczej. 
+        Właśnie wtedy zdała sobie sprawę, że częściej powinna wychodzić na ciastko.
+        <br><br>
+        W końcu przekroczyliśmy próg 50%, co oznacza, że jesteśmy bliżej niż dalej! Tłumaczenie idzie całkiem sprawnie, a nie mamy zamiaru zwalniać tempa. 
+        Tłumacze wylewają swoje siódme poty, korektorzy ich asekurują, a testerzy patrzą się i śmieją z błędów poprzedników.<br>
+        Przypominamy o naszym serwerze na <a href="https://discord.gg/8JTc36HNyM" class="text-red-500">Discordzie</a>, 
+        gdzie możecie zadać nam pytanie bądź chillować z innymi fanami omori. Lekarze również zalecają obczaić nasze konto na 
+        <a href="https://www.tiktok.com/@studio_kielkun" class="text-red-500">TikToku</a> oraz 
+        kanał na <a href="https://www.youtube.com/@studio-kielkun" class="text-red-500">YouTubie</a>.
+        <br><br>
+        Wi-elo!`,
+        photos: [
+            '4.1.png',
+            '4.2.png',
+            '4.3.png'
+        ]
+    },
+    {
         date: '25.01.2023',
         author: 'Skejven',
         text: `Siemka,<br>
         Dawno nie było żadnego newsa, więc pomyślałem, że to dobra pora na małą aktualizację.<br>
         Jeżeli chodzi o tłumaczenie, to aktualny stan przetłumaczonego tekstu to ponad 40%! Zbliżamy się wielkimi krokami do połowy i z tej 
         okazji mamy również dla was parę niespodzianek, które zostaną w swoim czasie ujawnione na naszym discordzie (na którego serdecznie zapraszamy). 
-        Od ostatniego czasu testerzy tłumaczenia nie próżnowali i bardzo pomagają nam wykrywać błędy, czy to w dialogach, walce czy przetłumaczonych grafikach.<br>
+        Od ostatniego czasu testerzy tłumaczenia nie próżnowali i bardzo pomagają nam wykrywać błędy, czy to w dialogach, walce czy przetłumaczonych grafikach.
+        <br><br>
         Kontynuując trend tworzenia naszych playlist, Oskar przygotował <a href="https://www.youtube.com/watch?v=M-1xScJcRp8" class="text-red-500">kolejną</a>
         składankę utworów wybranych przez członków naszego discorda. Tym razem tematem były święta, a to tylko pierwsza część! Następna ukaże się niedługo, 
         więc obserwujcie nasz kanał na youtube (jak zwykle, linki znajdują się na dole strony).<br> Nasz zespół życzy Wam dobrego roku 2023, 
-        oby był to rok w którym OMORI będzie można przejść w całości po polsku. <br>
+        oby był to rok w którym OMORI będzie można przejść w całości po polsku.
+        <br><br>
         Pozdro i z essą!`,
         photos: [
             '3.1.png',
@@ -32,7 +58,7 @@ export var articles: NewsArticle[] = [
         <a href="https://www.youtube.com/watch?v=LBwL6BB1jjs" class="text-red-500">playlistę</a> z piosenkami wybranymi przez członków naszego zespołu. 
         Po przesłuchaniu sprawdźcie też nasz <a href="https://www.youtube.com/channel/UCUadiPvPzoAu6bcsV1BXo0w" class="text-red-500">kanał</a> youtube,
          mogą się tam co jakiś czas ukazywać ciekawe materiały!
-        <br>
+        <br><br>
         Do zobaczenia!`,
         photos: [
             '2.1.png',
@@ -54,7 +80,7 @@ export var articles: NewsArticle[] = [
             że ten procent wcale nie jest równy stopniowi ukończenia tłumaczenia. W to wchodzi bardzo wiele różnych czynników. Nasi graficy pracują, 
             aby język polski zawitał również na kolorowych obrazkach widocznych w samej grze, nie tylko w tekście. 
             Technicy (ja i Piotruś) ogarniają kod gry, tak, żeby sporo tekstu, minigierek i innych czynności w grze pasowało do naszego języka.
-            <br> 
+            <br><br>
             To tyle na razie!`,
         photos: [
             '1.1.jpg',
